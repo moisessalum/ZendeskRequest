b31b47293b47fb04a93f81c0e1eaa6d439565610
Moises Salum <moisessalum@M2.local>
2024-02-11 20:15:29 -0600

    Get env

--- src/main/scala/zendesk/Zendesk.scala
+++ src/main/scala/zendesk/Zendesk.scala
@@ -1,15 +1,14 @@
 package zendesk
 
-import sttp.client4._
+import sttp.client4.quick._
 import sttp.client4.upicklejson.default._
 import upickle.default._
 
-
 object Zendesk extends App {
-  private val username = ""
-  private val password = ""
+  val username: String = sys.env("ZENDESK_USERNAME")
+  val password: String = sys.env("ZENDESK_PASSWORD")
 
-  val response: Response[String] = quickRequest
+  val response = quickRequest
     .get(uri"https://support-digicel-bb-play.zendesk.com/api/v2/search.json?query=type%3Aticket+status%3Aopen&brand%3A\"Barbados\"&sort_by=created_at&sort_order=asc")
     .auth.basic(user = username, password = password)
     .send()
